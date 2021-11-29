0b5ac11e78cc10bd3880ece83763b4489af8dd7b
Nata2401 <kuzyan.rd@yandex.ru>
2021-11-29 16:46:57 +0300

    Выполнено усложненное задание к 3-му уроку

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*jshint esversion: 9 */
 const num = 266219;
-String(num);
-Array.from(String(num));
 
 const myArray = Array.from(String(num)).map(i=>Number(i));
 console.log(myArray);
@@ -14,3 +12,34 @@ console.log(deg);
 
 console.log(String(deg).substr(0,2)); 
 
+
+let lang = 'en';
+if (lang === 'ru') {
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+} else {
+    console.log('Некорректное значение');
+}
+
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        break;
+    default:
+        console.log('Некорректное значение');
+}
+
+let langArr = [];
+langArr.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+langArr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(langArr[lang]);
+
+let namePerson = '';
+
+console.log((namePerson === 'Артем') ? 'директор' :
+    (namePerson === 'Александр') ? 'преподаватель' :
+    'студент');
