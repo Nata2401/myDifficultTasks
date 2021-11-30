bb7445c88970d17f73649367476452aa3ae6f57b
Nata2401 <kuzyan.rd@yandex.ru>
2021-11-30 19:16:05 +0300

    Выполнено усложненное задание к уроку 5

--- script.js
+++ script.js
@@ -43,3 +43,21 @@ let namePerson = '';
 console.log((namePerson === 'Артем') ? 'директор' :
     (namePerson === 'Александр') ? 'преподаватель' :
     'студент');
+
+
+const foo = (arg) => {
+    let res = 'Ожидается появление строки...';
+    if (typeof arg === "string") {
+        const maxLength = 30;
+        const str = arg.trim();
+        if (str.length > maxLength) {
+            res = str.substr(0,maxLength) + '...';
+        } else {
+            res = str;
+        }
+    }
+    return res;
+};
+console.log('foo(): ', foo(5));
+console.log('foo(): ', foo('    Строка менее 30 символов     '));
+console.log('foo(): ', foo('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
